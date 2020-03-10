87122473749fe93531a4b80ad1ae0330127f47b1
Geovane Chaves <geovane.pires@floatbrasil.com.br>
2020-03-10 15:07:31 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ViverRenda />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ViverRenda from './components/ViverRenda.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ViverRenda
   }
 }
 </script>
---
+++ src/components/ViverRenda.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <h1>Calculadora Viver de Renda</h1>
+    <p>Quanto eu preciso para alcancar minha liberdade financeira: </p>
+    <div>
+        <label>Quanto voce quer ganhar por mês? </label>
+        <input v-model="ganhar" type="text" />
+    </div>
+
+    <div>
+        <label>Qual o tipo de aplicacao: Renda Fixa - Tesouro Direto</label>
+    </div>
+
+    <hr />
+
+    <div v-if="isDev" class="dividir-abas-outras-aplicacoes">
+      
+      <div>
+          <label>Deposito Inicial: </label>
+          <input type="text" />
+      </div>
+
+      <div>
+          <label>Deposito Mensal: </label>
+          <input type="text" />
+      </div>
+
+
+      <div>
+          <label>Tempo de investimento: </label>
+          <input type="text" />
+      </div>
+
+    </div>
+
+    <button @click="calcular">Calcular</button>
+
+    <p>Você precisará de R$: {{ formatPrice(resultado) }} investido.</p>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViverRenda',
+  props: {
+    msg: String
+  },
+  data: function() {
+      return {
+          resultado: 3,
+          ganhar: 0,
+          isDev: false,
+          tiposInvestimento: [
+                { text: 'Tesouro Direto', value: '1' },
+                { text: 'Fundos Imobiliarios', value: '2' },
+            ]
+      }
+  },
+  methods: {
+      calcular(){
+          this.resultado = this.ganhar / (0.4 / 100)
+      },
+      formatPrice(value) {
+        let val = (value/1).toFixed(2).replace('.', ',')
+        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
